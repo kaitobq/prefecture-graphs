26774bd247786360ced9826b4a7a91ab85cdc228
kaitobq <kaitobq520@gmail.com>
2024-07-31 08:59:48 +0900

    initialize

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+    "semi": false,
+    "doubleQuote": true,
+    "printWidth": 90,
+    "tabWidth": 2,
+    "trailingComma": "all",
+    "jsxDoubleQuote": true
+}
