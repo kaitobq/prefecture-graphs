6d4fb9299c86262bf1a82bdbf6cfd2727b7df42b
kaitobq <kaitobq520@gmail.com>
2024-08-05 00:49:42 +0900

    ページコンポーネントにグラフとチェックボックスを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,27 @@
+"use client"
+
 import styles from "./page.module.css"
+import { CheckBoxList } from "@/components/checkboxList"
+import { usePrefecture } from "@/utils/hooks"
+import { useState } from "react"
+import { Graph } from "@/components/graph"
 
 export default function Home() {
-  return <main className={styles.main}>prefecture graphs</main>
+  const { prefectures, isLoading, error } = usePrefecture()
+  const [prefCodes, setPrefCodes] = useState<number[]>([])
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.graph}>
+        <Graph prefCodes={prefCodes} label="総人口" prefectures={prefectures} />
+      </div>
+      <div className={styles.checkboxList}>
+        <CheckBoxList
+          prefectures={prefectures}
+          setPrefCodes={setPrefCodes}
+          prefCodes={prefCodes}
+        />
+      </div>
+    </main>
+  )
 }
